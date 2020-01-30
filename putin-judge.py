08fe112e26cdbe425aba344a47eba154e41a989a
cz1203aty <742043564@qq.com>
2020-01-30 23:27:31 +0800

    Update putin-judge.py

--- putin-judge.py
+++ putin-judge.py
@@ -10,6 +10,12 @@ def judgeword(w):
      else:
           print("不符合，请重新请输入单词")
           aa=putin()
+def countword(w):
+     res={}
+     for i in w:
+          res[i]=w.count(i)
+          c=len(res)
+     print("输入的字符串中共有%d个不同的字母" %c)
 aa=putin()
 judgeword(aa)
-
+countword(aa)
